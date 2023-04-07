afa62c3af1cf4d90efe78e73a5078f1b518e1dd8
Yared <yared.riveros@utec.edu.pe>
2023-04-07 11:51:16 -0500

    P4 terminada-sin empaquetar

--- P4/fixedRecord.h
+++ P4/fixedRecord.h
@@ -6,22 +6,6 @@
 using namespace std;
 
 
-void writeToFile(const char** arr, int size, const char* fileName){
-    ofstream file(fileName, ios::out | ios::binary);
-    file.write((char*) &size, sizeof(size));
-    file.write((char*) arr, sizeof(char*) * size); //escribo paquete de atributos de texto
-    file.close();
-}
-
-void readFromFile(char**& arr, int& size, const char* fileName){
-    ifstream file(fileName, ios::in | ios::binary);
-    file.read((char*) &size, sizeof(size));
-    arr = new char*[size];
-    file.read((char*) arr, sizeof(char*) * size);
-    file.close();
-}
-
-
 class FixedRecord{
 private:
     string fileName;
@@ -32,26 +16,27 @@ public:
     }
 
     void add(Matricula m){
-        ofstream file(fileName, ios::out | ios::binary | ios::app);
+        fstream file(fileName,ios::out | ios::binary | ios::app);
         //1. Anoto su posición al final del header
         ofstream header(headerName, ios::out | ios::binary | ios::app);
-        size_t pos = file.tellp();
-        header.write((char*)&pos, sizeof(header.tellp()));
+        int pos = file.tellp();
+        header.write((char*)&pos, sizeof(pos));
         header.close();
         //2. Escribo la matricula al final del archivo
-        int size = m.getSize();
-        const char** arr = m.empaquetar(); //obtengo el array de atributos de texto
 
-        //Escribo el paquete de atributos de texto
-        file.write((char*) &size, sizeof(size));
-        file.write((char*) arr, sizeof(char*) * size); //escribo paquete de atributos de texto
-        cout << "arrays(escribo): " << endl;
-        for(int i = 0; i < size; i++){
-            cout << arr[i] << endl;
-        }
-        //Escribo los atributos de tipo numérico
+        //Escribo los atributos
+        ///Codigo
+        int lenCodigo = m.codigo.length();
+        file.write((char*) &lenCodigo, sizeof(lenCodigo));
+        file.write(m.codigo.c_str(), lenCodigo);
+        ///Ciclo
         file.write((char*) &m.ciclo, sizeof(m.ciclo));
+        ///Mensualidad
         file.write((char*) &m.mensualidad, sizeof(m.mensualidad));
+        ///Observaciones
+        int lenObservaciones = m.observaciones.length();
+        file.write((char*) &lenObservaciones, sizeof(lenObservaciones));
+        file.write(m.observaciones.c_str(), lenObservaciones);
         file.close();
     }
 
@@ -63,24 +48,31 @@ public:
         int fin = file.tellg();
         file.seekg(0, ios::beg);
         while(file.tellg() < fin){ //Si no he llegado al final del archivo
-            // file.seekg(0, ios::beg); //Ya que comprobé que no está vacío, me muevo al inicio
-            char** newArr;
-            int newSize;
             Matricula m;
-            //Leo el paquete de atributos de texto
-            // readFromFile(newArr, newSize, fileName.c_str());
-            file.read((char*) &newSize, sizeof(newSize));
-            newArr = new char*[newSize];
-            file.read((char*) newArr, sizeof(char*) * newSize);
-            m.desempaquetar(newArr);
-            //Leo los atributos de tipo numérico
+            //Leo los atributos
+            ///Codigo
+            int lenCodigo;
+            file.read((char*) &lenCodigo, sizeof(lenCodigo));
+            char* codigo = new char[lenCodigo];
+            file.read(codigo, lenCodigo);
+            codigo[lenCodigo] = '\0';
+            m.codigo = codigo;
+            ///Ciclo
             file.read((char*) &m.ciclo, sizeof(m.ciclo));
+            ///Mensualidad
             float value;
             char buffer[sizeof(float)];
             file.read((char*) buffer, sizeof(float));
             value = *(float*) buffer;
             m.mensualidad = value;
-
+            ///Observaciones
+            int lenObservaciones;
+            file.read((char*) &lenObservaciones, sizeof(lenObservaciones));
+            char* observaciones = new char[lenObservaciones];
+            file.read(observaciones, lenObservaciones);
+            observaciones[lenObservaciones] = '\0';
+            m.observaciones = observaciones;
+            //Agrego la matricula al vector
             matriculas.push_back(m);
         }
         file.close();
@@ -91,15 +83,43 @@ public:
         //1. Abro el header
         ifstream file(headerName, ios::in | ios::binary);
         //2. Me muevo a la posición pos
-        file.seekg(pos * sizeof(file.tellg())); //sizeof(file.tellg()) es el tamaño de toda posición escrita en el header
+        file.seekg(pos * sizeof(int),ios::beg); //pos * sizeof(int) porque cada posición del header es un int
         //3. Leo la posición del registro
-        char **newArr;
-        int newSize;
-        Matricula m;
-        readFromFile(newArr, newSize, fileName.c_str());
-        m.desempaquetar(newArr);
-        //4. Cierro el header
+        int posRegistro;
+        file.read((char*) &posRegistro, sizeof(posRegistro));
+        // //4. Cierro el header
         file.close();
+        //5. Abro el archivo
+        ifstream file2(fileName, ios::in | ios::binary);
+        //6. Me muevo a la posición del registro
+        file2.seekg(posRegistro, ios::beg);
+        //7. Leo el registro
+        Matricula m;
+        // Leo los atributos
+        /// Codigo
+        int lenCodigo;
+        file2.read((char *)&lenCodigo, sizeof(lenCodigo));
+        char *codigo = new char[lenCodigo];
+        file2.read(codigo, lenCodigo);
+        codigo[lenCodigo] = '\0';
+        m.codigo = codigo;
+        /// Ciclo
+        file2.read((char *)&m.ciclo, sizeof(m.ciclo));
+        /// Mensualidad
+        float value;
+        char buffer[sizeof(float)];
+        file2.read((char *)buffer, sizeof(float));
+        value = *(float *)buffer;
+        m.mensualidad = value;
+        /// Observaciones
+        int lenObservaciones;
+        file2.read((char *)&lenObservaciones, sizeof(lenObservaciones));
+        char *observaciones = new char[lenObservaciones];
+        file2.read(observaciones, lenObservaciones);
+        observaciones[lenObservaciones] = '\0';
+        m.observaciones = observaciones;
+        //8. Cierro el archivo
+        file2.close();
 
         return m;
     }
--- P4/main.cpp
+++ P4/main.cpp
@@ -28,7 +28,7 @@ int main(){
     cout << "\t--Imprimo matrículas registradas hasta el momento--" << endl;
     vector<Matricula> matriculas = f.load();
     for(int i=0;i<matriculas.size();i++){
-        cout << "Matrícula " << i+1 << endl;
+        cout << "Matrícula " << i << endl;
         cout << "Código: " << matriculas[i].codigo << endl;
         cout << "Ciclo: " << matriculas[i].ciclo << endl;
         cout << "Mensualidad: " << matriculas[i].mensualidad << endl;
@@ -46,18 +46,18 @@ int main(){
     m.observaciones = "Un capo";
     f.add(m);
 
-    // //Nueva matricula 3
-    // cout << "Agrego la matrícula 3" << endl;
-    // m.codigo = "2020-1";
-    // m.ciclo = 2;
-    // m.mensualidad = 200;
-    // m.observaciones = "Cuídense de este";
-    // f.add(m);
+    //Nueva matricula 3
+    cout << "Agrego la matrícula 3" << endl;
+    m.codigo = "2020-1";
+    m.ciclo = 2;
+    m.mensualidad = 200;
+    m.observaciones = "Cuídense de este";
+    f.add(m);
 
     cout << "\t--Imprimo matrículas registradas hasta el momento--" << endl;
     matriculas = f.load();
     for(int i=0;i<matriculas.size();i++){
-        cout << "Matrícula " << i+1 << endl;
+        cout << "Matrícula " << i << endl;
         cout << "Código: " << matriculas[i].codigo << endl;
         cout << "Ciclo: " << matriculas[i].ciclo << endl;
         cout << "Mensualidad: " << matriculas[i].mensualidad << endl;
@@ -65,22 +65,15 @@ int main(){
         cout << "-----------------------------------------------" << endl;
     }
 
-    // cout << "\t--Leo la matrícula en la posición 1--" << endl;
-    // Matricula m2 = f.readRecord(1);
-    // cout << "Matrícula 1:" << endl << endl;
-    // cout << "Código: " << m2.codigo << endl;
-    // cout << "Ciclo: " << m2.ciclo << endl;
-    // cout << "Mensualidad: " << m2.mensualidad << endl;
-    // cout << "Observaciones: " << m2.observaciones << endl;
-    // cout << "-----------------------------------------------" << endl;
+    cout << "\t--Leo la matrícula en la posición 2 (contando desde 0)--" << endl;
+    Matricula m2 = f.readRecord(2);
+    cout << "Matrícula seleccionada:" << endl << endl;
+    cout << "Código: " << m2.codigo << endl;
+    cout << "Ciclo: " << m2.ciclo << endl;
+    cout << "Mensualidad: " << m2.mensualidad << endl;
+    cout << "Observaciones: " << m2.observaciones << endl;
+    cout << "-----------------------------------------------" << endl;
 
 
-
-    //errores:
-    //1. El add agrega registros, pero no concuerdan con el código u observaciones ingresadas. Parece que se debe a que si cambio
-    //el char** arr y lo escribo, se modifican todos los arrays escritos en el archivo. Por ello todas las matrículas tienen el
-    //mismo código y observaciones. Sin embargo, esto no pasa con los enteros y floats porque los inserté directamente en el archivo.
-
-    //La posible solución es dejar de intentar empaquetar los strings y escribirlos directamente en el archivo. 
     return 0;
 }
